3499edb419a95a0c8ee16653905607837d630f9b
Manjunath CV <cvmanjoo@gmail.com>
2024-08-04 21:43:35 +0530

    Small Changes

--- index.php
+++ index.php
@@ -52,9 +52,7 @@ if(filter_has_var(INPUT_GET, 'submit'))
 		<title>Magnet</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
 		<link rel="icon" type="image/png" sizes="32x32" href="magnet.png">
-
 		<link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" type="text/css" href="https://www.w3schools.com/lib/w3-theme-red.css">
 		<link rel="stylesheet" type="text/css" href="style.css">
